047eedc4bf5ed05eb342a6f228bae1a8a03073c0
dkim157 <63015027+dkim157@users.noreply.github.com>
2022-09-27 10:29:21 -0700

    read and write image

--- assignment2/ECB.py
+++ assignment2/ECB.py
@@ -1,4 +1,14 @@
+from PIL import Image
+import numpy as np
 
+def read_image():
+    img_arr = Image.open('./mustang.bmp')
+    img_arr = np.array(Image.open('./mustang.bmp'))
+    return img
+
+def write_image(img_arr):
+    im = Image.fromarray(img_arr)
+    im.save("ECB_encrypted.jpeg")
 
 if __name__ == '__main__':
     pass
