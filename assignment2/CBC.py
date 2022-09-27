6ed08f6d3945e0974df4c9a3c70e97bafca3e824
dkim157 <63015027+dkim157@users.noreply.github.com>
2022-09-27 10:32:22 -0700

    read and write image

---
+++ assignment2/CBC.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import numpy as np
+
+def read_image():
+    img_arr = Image.open('./mustang.bmp')
+    img_arr = np.array(Image.open('./mustang.bmp'))
+    return img_arr
+
+def write_image(img_arr):
+    im = Image.fromarray(img_arr)
+    im.save("CBC_encrypted.jpeg")
+
+if __name__ == '__main__':
+    pass
