af1578800983fe09672e4bb88bdbada941bd80c6
dkim157 <63015027+dkim157@users.noreply.github.com>
2022-09-27 10:45:49 -0700

    read and write bmp

--- assignment2/CBC.py
+++ assignment2/CBC.py
@@ -2,13 +2,17 @@ from PIL import Image
 import numpy as np
 
 def read_image():
-    img_arr = Image.open('./mustang.bmp')
-    img_arr = np.array(Image.open('./mustang.bmp'))
-    return img_arr
+    file = open("mustang.bmp","rb")
+    data = file.read()
+    file.close()
+    return data
 
-def write_image(img_arr):
-    im = Image.fromarray(img_arr)
-    im.save("CBC_encrypted.jpeg")
+def write_image(data):
+    file = open("CBC_encrypted.bmp","wb")
+    file.write(data)
+    file.close()
 
 if __name__ == '__main__':
-    pass
+    img = read_image()
+    img_header = img[0:14] #header is in this portion of bytes
+    #write_image(img)
