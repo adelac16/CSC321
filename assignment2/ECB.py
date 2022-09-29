3d9fac96e59fcbf18c607f358d22725c7e88e1c8
adelac16 <adelac16@calpoly.edu>
2022-09-29 10:13:54 -0700

    cleaning and fixing block data

--- assignment2/ECB.py
+++ assignment2/ECB.py
@@ -8,31 +8,29 @@ from Crypto.Util.Padding import unpad
 
 
 def read_image():
-    file = open("mustang.bmp","rb")
+    file = open("mustang.bmp", "rb")
     data = file.read()
     file.close()
     return data
 
 def write_image(data):
-    file = open("ECB_encrypted.bmp","wb")
+    file = open("ECB_encrypted.bmp", "wb")
     file.write(data)
     file.close()
 
 
-def encrypt_text(data, key):
+def encrypt_block(data, key):
+    if type(data) != bytes:
+        data = data.encode('UTF-8')
     cipher = AES.new(key, AES.MODE_ECB)
     ct_bytes = cipher.encrypt(pad(data, AES.block_size))
     ct = b64encode(ct_bytes).decode('utf-8')
     result = json.dumps({'ciphertext': ct})
     print(result)
-    #
-    # cipher = AES.new(key, AES.MODE_ECB)
-    # print(cipher)
-    # ciphertext = cipher.encrypt(text)
     return (result)
 
 
-def decrypt_text(json_input, key):
+def decrypt_block(json_input, key):
     try:
         b64 = json.loads(json_input)
         ct = b64decode(b64['ciphertext'])
@@ -47,7 +45,7 @@ if __name__ == '__main__':
     img = read_image()
     img_header = img[0:54]
     key = get_random_bytes(16)
-    # text = input("input a string to encrypt: ")
-    data = b"secret"
-    jsonCt = encrypt_text(data, key)
-    decrypt_text(jsonCt, key)
+    text = input("input a string to encrypt: ")
+    data = text # b"secret"
+    jsonCt = encrypt_block(data, key)
+    decrypt_block(jsonCt, key)
