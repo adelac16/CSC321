5b2597f3f7ef9cd099ff201ed8a6e03e03b59ebf
adelac16 <adelac16@calpoly.edu>
2022-09-29 17:10:38 -0700

    comment out decrypt and implemented cbc

--- assignment2/CBC.py
+++ assignment2/CBC.py
@@ -14,7 +14,7 @@ def read_image():
     return data
 
 def write_image(data):
-    file = open("ECB_encrypted.bmp", "wb")
+    file = open("CBC_encrypted.bmp", "wb")
     file.write(data)
     file.close()
 
@@ -26,34 +26,64 @@ def encrypt_block(data, key):
     ct_bytes = cipher.encrypt(pad(data, AES.block_size))
     ct = b64encode(ct_bytes).decode('utf-8')
     result = json.dumps({'ciphertext': ct})
-    print(result)
-    return result
+    return result.encode('UTF-8')
 
 
-def decrypt_block(json_input, key):
-    try:
-        b64 = json.loads(json_input)
-        ct = b64decode(b64['ciphertext'])
-        cipher = AES.new(key, AES.MODE_ECB)
-        pt = unpad(cipher.decrypt(ct), AES.block_size)
-        print("The message was: ", pt)
-    except (ValueError, KeyError):
-        print("Incorrect decryption")
+# def decrypt_block(json_input, key):
+#     try:
+#         b64 = json.loads(json_input)
+#         ct = b64decode(b64['ciphertext'])
+#         cipher = AES.new(key, AES.MODE_ECB)
+#         pt = unpad(cipher.decrypt(ct), AES.block_size)
+#         print("The message was: ", pt)
+#     except (ValueError, KeyError):
+#         print("Incorrect decryption")
+#
+#
+# def encrypt_decrypt_block(data, key):
+#     jsonCt = encrypt_block(data, key)
+#     decrypt_block(jsonCt, key)
 
 
-def encrypt_decrypt_block(data, key):
-    jsonCt = encrypt_block(data, key)
-    decrypt_block(jsonCt, key)
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+
+def CBC_block(data, vector, key):
+    # print(data)
+    xor_data = byte_xor(data, vector)
+    enc_block = encrypt_block(xor_data, key)
+    return enc_block
 
 
 def CBC_image(img_body):
-    pass
+    key = get_random_bytes(16)
+    IV = get_random_bytes(128)
+    enc_image = []
+    start = 0
+    end = start + 16
+    while start < len(img_body):
+        enc_block = CBC_block(IV, img_body[start:end], key)
+        IV = enc_block
+        enc_image.append(enc_block)
+        start += 16
+        end = start + 16
+        if end > len(img_body):
+            end = len(img_body)
+    return enc_image
+
 
 
 if __name__ == '__main__':
     img = read_image()
     img_header = img[0:54]
-    key = get_random_bytes(16)
-    inText = input("input a string to encrypt: ")
-    data = inText
-    encrypt_decrypt_block(data, key)
+    # inText = input("input a string to encrypt: ")
+    # data = inText
+    enc_image_list = CBC_image(img[55:-1])
+    enc_image_list.insert(0, img_header)
+    file = open("CBC_encrypted.bmp", "wb")
+    print("writing to file")
+    for i in enc_image_list:
+        file.write(i)
+    file.close()
+
