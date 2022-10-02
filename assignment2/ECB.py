43ceda75b95e7e48441be30269af136639e0fbb1
dkim157 <63015027+dkim157@users.noreply.github.com>
2022-10-02 14:55:20 -0700

    ECB no pad

--- assignment2/ECB.py
+++ assignment2/ECB.py
@@ -38,33 +38,20 @@ def encrypt_block(data, key):
     if type(data) != bytes:             # checks if data is already bytes
         data = data.encode('UTF-8')     # converts data to bytes
     cipher = AES.new(key, AES.MODE_ECB)
-    ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+    ct_bytes = cipher.encrypt(data)
     return ct_bytes
 
 
 def encrypt_img(header, body, key):
-    enc_img = []
-    start_idx = 0
-    end_idx = 16
-    #encrypt one block at a time
-    while start_idx < len(body):
-        # encrypt block and append to result
-        enc_block = encrypt_block(body[start_idx:end_idx], key)
-        enc_img.append(enc_block)
-        # increment indices
-        start_idx += 16
-        end_idx += 16
-        # padding for last block
-        if end_idx > len(body):
-            pad = b''
-            pad_len = 16-(end_idx-len(body)) # length of pad
-            # add required # of bytes
-            for i in range(pad_len):
-                pad += bytes(pad_len)
-            print(pad) # is this even right
-            end_idx = len(body)
-    enc_img.insert(0, header)
-    return enc_img
+    enc_img = b''
+    n = 16 # chunk length
+    blocks = [body[i:i+n] for i in range(0, len(body), n)]
+    for block in blocks:
+        if len(block)<16:
+            block = pad(block, 16)
+        enc_block = encrypt_block(block, key)
+        enc_img += enc_block
+    return (header + enc_img)
 
 if __name__ == '__main__':
     # read image
@@ -74,10 +61,8 @@ if __name__ == '__main__':
     body = img[54:]
     key = get_random_bytes(16)
     # encrypt
-    enc_img_list = encrypt_img(header, body, key)
-    #print(enc_img_list)
+    enc_img = encrypt_img(header, body, key)
     # write to result
-    #data = b''.join(enc_img_list)
-    #print('writing image')
-    #write_image(data)
+    print('writing image')
+    write_image(enc_img)
 
