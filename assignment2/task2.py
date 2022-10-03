605ace9b0cab0021ee48cbfced8c938c2d081b01
adelac16 <adelac16@calpoly.edu>
2022-10-02 19:37:49 -0700

    fixed cbc, working submit, working verify

--- assignment2/task2.py
+++ assignment2/task2.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
+import urllib.parse
 from base64 import b64decode
 from Crypto.Util.Padding import unpad
 
@@ -10,6 +11,7 @@ def read_image():
     data = file.read()
     file.close()
     return data
+
 
 def write_image(data):
     file = open("CBC_encrypted.bmp", "wb")
@@ -43,7 +45,7 @@ def CBC_encrypt(body, key, IV):
     blocks = [body[i:i + n] for i in range(0, len(body), n)]
     for block in blocks:
         enc_block = encrypt_block(block, key, IV)
-        IV = enc_block
+        IV = enc_block          # changes vector to encrypted ciphertext block
         enc += enc_block
     return enc
 
@@ -51,9 +53,8 @@ def CBC_encrypt(body, key, IV):
 def decrypt_block(ct, key, vector):
     try:
         cipher = AES.new(key, AES.MODE_ECB)
-        pt = cipher.decrypt(ct)
-        pt = byte_xor(pt, vector)
-        pt = unpad(pt, 16)
+        pt = cipher.decrypt(ct)             # decrypts with key
+        pt = byte_xor(pt, vector)           # xor with vector
         # print("The message was: ", pt)
         return pt
     except (ValueError, KeyError):
@@ -65,19 +66,50 @@ def CBC_decrypt(body, key, IV):
     n = 16
     blocks = [body[i:i + n] for i in range(0, len(body), n)]
     for block in blocks:
-        dec_block = decrypt_block(block, key, IV)
-        IV = block
+        dec_block = decrypt_block(block, key, IV)   #decrypts current block
+        IV = block          # changes vector to previous ciphertext block
+        if dec_block == blocks[-1]:
+            dec_block = unpad(dec_block, 16)
         dec += dec_block
     return dec
 
 
+def parse_data(data):
+    data = data.replace(";", urllib.parse.quote(";"))
+    data = data.replace("=", urllib.parse.quote("="))
+    return data
+
+
+def submit(key, IV):
+    prepend = "userid=456;userdata="
+    append = ";session-id=31337"
+    data =input("What is your message? ")
+    data = parse_data(data)
+    data = prepend + data + append
+    ct = CBC_encrypt(data, key, IV)
+    return ct
+
+
+def verify(data, key, IV):
+    pt = CBC_decrypt(data, key, IV)
+    print(pt)
+    return b';admin=true' in pt
+
+def bit_flip(ct):
+    n = 16
+    blocks = [ct[i:i + n] for i in range(0, len(ct), n)]
+
+
+
 if __name__ == '__main__':
-    key = get_random_bytes(16)
-    IV = get_random_bytes(16)
-    data = input("input a string to encrypt: ")
-    enc = encrypt_block(data, key, IV)
-    print(f'encrypted string: {enc}')
-    dec = decrypt_block(enc, key, IV)
-    print(f'decrypted string: {dec}')
+    key = get_random_bytes(16)      # same key throughout whole program
+    IV = get_random_bytes(16)       # same IV throughout whole program
+    # enc = CBC_encrypt(data, key, IV)
+    # print(f'encrypted string: {enc}')
+    # dec = CBC_decrypt(enc, key, IV)
+    # print(f'decrypted string: {dec}')
+    ct = submit(key, IV)
+    print(ct)
+    print(f'admin? {verify(ct, key, IV)}')
 
 
